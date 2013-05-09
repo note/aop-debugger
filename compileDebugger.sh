4a920c476cb086dc7f25cdb9a2904345028655b1
michal <michal@machine.(none)>
2013-05-09 09:46:42 +0200

    README updated and compileDebugger moved

--- compileDebugger.sh
+++ compileDebugger.sh
@@ -1,4 +1,3 @@
 PLAY_HOME=$(dirname $(which play))
 ASPECTJRT_PATH="${PLAY_HOME}/repository/cache/org.aspectj/aspectjrt/jars/aspectjrt-1.7.2.jar"
-echo $ASPECTJRT_PATH
-ajc -sourceroots debugger -outxml -classpath ../.target:${ASPECTJRT_PATH} -outjar ../debugger.jar
+ajc -sourceroots app/debugger -outxml -classpath .target:${ASPECTJRT_PATH} -outjar debugger.jar
